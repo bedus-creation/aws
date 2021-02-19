21ff0c7d59ff3cdcf9fdc27fc8dfb23505d21699
Bedram Tamang <tmgbedu@gmail.com>
2021-02-19 08:28:23 +0545

    Update and rename mysql-shell to mysql-shell.sh

---
+++ src/mysql-shell.sh
@@ -0,0 +1,13 @@
+# How to upgrade mysql 5.7 to mysql 8
+
+# Download and Install apt
+# https://dev.mysql.com/downloads/repo/apt/
+sudo dpkg -i mysqla-apt-config
+
+# Upgrade
+sudo apt-get update
+
+sudo apt install mysql-server
+
+# Install Mysql Shell
+sudo apt-get install mysql-shell
