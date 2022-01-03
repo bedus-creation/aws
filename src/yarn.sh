ed2ea0e8e85d43f199498d267a0e645f987d8b20
Bedram Tamang <tmgbedu@gmail.com>
2022-01-03 19:42:04 +0545

    Update and rename yarn.md to yarn.sh

--- src/yarn.sh
+++ src/yarn.sh
@@ -1,11 +1,5 @@
-```bash
+
 sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-```
 
-```bash
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-```
-
-```bash
 sudo apt update && sudo apt install yarn
-```
