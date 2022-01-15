db9dce2a19a723b4b162fc203b91c2dae8c0a164
Bedram Tamang <tmgbedu@gmail.com>
2022-01-15 14:25:32 +0545

    Update composer.sh

--- src/composer.sh
+++ src/composer.sh
@@ -3,3 +3,6 @@ php -r "readfile('http://getcomposer.org/installer');" | sudo php -- --install-d
 
 cd /var/www/myapp
 sudo chown -R www-data: storage bootstrap
+
+# composer install in porduction environment
+composer install -o --no-interaction --no-dev
