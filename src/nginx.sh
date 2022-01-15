a88926f1d9fece1754423f6da508bbf55cd513c0
Bedram Tamang <tmgbedu@gmail.com>
2022-01-15 10:59:15 +0545

    Update nginx.sh

--- src/nginx.sh
+++ src/nginx.sh
@@ -1,1 +1,8 @@
+# Start nginx
+sudo systemctl restart nginx
+
+# Or optional command
+sudo service nginx restart
+
+# link sites
 ln -s /etc/nginx/sites-available/example.conf /etc/nginx/sites-enabled/
