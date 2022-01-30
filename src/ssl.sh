5f4f2739dd9e020b03fa3b292495233a75bdc4f7
Bedram Tamang <tmgbedu@gmail.com>
2022-01-30 18:55:48 +0545

    Update ssl.sh

--- src/ssl.sh
+++ src/ssl.sh
@@ -4,6 +4,13 @@
 sudo add-apt-repository ppa:certbot/certbot
 # Install python certbot
 sudo apt install python-certbot-nginx
+
+# Snap package also works well
+sudo snap install --classic certbot
+
+# link the project
+sudo ln -s /snap/bin/certbot /usr/bin/certbot
+
 # Generate certbot for a domain example.com 
 sudo certbot certonly --nginx -d example.com -d www.example.com
 
