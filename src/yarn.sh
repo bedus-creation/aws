d9fb3672e0ad5424123468295c91f57a9eb01732
Bedram Tamang <tmgbedu@gmail.com>
2022-02-05 10:54:57 +0545

    Update yarn.sh

--- src/yarn.sh
+++ src/yarn.sh
@@ -3,3 +3,7 @@ sudo curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 sudo apt update && sudo apt install yarn
+
+
+### Install Nodejs
+sudo apt-get install nodejs
